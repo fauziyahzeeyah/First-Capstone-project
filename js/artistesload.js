ae72ba8ff7ec4672cd17448e4049e2a50c634fd0
zeeyah <92362966+fauziyahzeeyah@users.noreply.github.com>
2023-01-14 10:54:10 +0100

    Corrected linter and eslint errors

--- js/artistesload.js
+++ js/artistesload.js
@@ -1,58 +1,58 @@
 const artisteData = [
   {
-    name: "Wizkid (Big wiz)",
-    proffesionalism: "Nigerian male artiste",
-    img: "images/wizkid img.jpg",
+    name: 'Wizkid (Big wiz)',
+    proffesionalism: 'Nigerian male artiste',
+    img: 'images/wizkid img.jpg',
     description:
       "Wizkid being the most famous musician in Nigeria  didn't just woke up and became famous, he started  from the scratch and now popularly known as BigWiz  worldwide.",
   },
   {
-    name: " Davido ",
-    proffesionalism: "Nigerian male artiste",
-    img: "images/davido img.jpg",
+    name: ' Davido ',
+    proffesionalism: 'Nigerian male artiste',
+    img: 'images/davido img.jpg',
     description:
-      " Davido born with a silver spoon but still decided to make his own money through his music career. Having a great vocals and friendly acts made his fans love him so much.",
+      ' Davido born with a silver spoon but still decided to make his own money through his music career. Having a great vocals and friendly acts made his fans love him so much.',
   },
   {
-    name: "Ayra Starr",
-    proffesionalism: "Nigerian female artiste",
-    img: "images/ayra img.jpg",
+    name: 'Ayra Starr',
+    proffesionalism: 'Nigerian female artiste',
+    img: 'images/ayra img.jpg',
     description:
-      "A rising star who began her music career at age 19 is now known worldwide and has sang with famous musicians worldwide. Her vocals are just too good.",
+      'A rising star who began her music career at age 19 is now known worldwide and has sang with famous musicians worldwide. Her vocals are just too good.',
   },
   {
-    name: "Omah Lay",
-    proffesionalism: "Nigerian male artiste",
-    img: "images/omah img.jpg",
+    name: 'Omah Lay',
+    proffesionalism: 'Nigerian male artiste',
+    img: 'images/omah img.jpg',
     description:
       "Omah lay a young artiste who drops good lyrics for his fans but just doesn't sing regularly yet loved by his fans.",
   },
   {
-    name: "Simi",
-    proffesionalism: "Nigerian female artiste",
-    img: "images/simi stage.jpg",
+    name: 'Simi',
+    proffesionalism: 'Nigerian female artiste',
+    img: 'images/simi stage.jpg',
     description:
       "Simi a well-talented young lady sings with passion whenever she's on stage and makes sure her fans got loads of fun.",
   },
   {
-    name: "Burna Boy",
-    proffesionalism: "Nigerian male artiste",
-    img: "images/Burna-Boy.jpg",
+    name: 'Burna Boy',
+    proffesionalism: 'Nigerian male artiste',
+    img: 'images/Burna-Boy.jpg',
     description:
-      " Burna boy popularly known as African Giant sings with  more than enough energy and makes sure there is no room for boredom.",
+      ' Burna boy popularly known as African Giant sings with  more than enough energy and makes sure there is no room for boredom.',
   },
 ];
 function allArtistesCards(artiste) {
-  const container = document.querySelector("#desktop_artistes");
-  const artistesCards = container.querySelectorAll(".col-6");
+  const container = document.querySelector('#desktop_artistes');
+  const artistesCards = container.querySelectorAll('.col-6');
 
-  for (let i = 0; i < artiste.length; i++) {
+  for (let i = 0; i < artiste.length; i += 1) {
     const artisteData = artiste[i];
     const artisteCard = artistesCards[i];
-    const img = artisteCard.querySelector("img");
+    const img = artisteCard.querySelector('img');
     img.src = artisteData.img;
     img.alt = artisteData.name;
-    const content = artisteCard.querySelector("p");
+    const content = artisteCard.querySelector('p');
     content.innerHTML = `
         <span class="name">${artisteData.name}</span>
         <br>
--- js/main.js
+++ js/main.js
@@ -1,43 +1,35 @@
+/* menu dropdown codes....... */
+const openHambuger = document.querySelector('.hambuger img');
+const menu = document.querySelector('.hide-menu');
+const closeButton = document.querySelector('.close_button');
+/* open button */
+openHambuger.addEventListener('click', () => {
+  menu.style.display = 'block';
+  openHambuger.style.display = 'none';
+  closeButton.style.display = 'block';
+});
 
+/* clossing button ... */
+closeButton.addEventListener('click', () => {
+  menu.style.display = 'none';
+  openHambuger.style.display = 'block';
+  closeButton.style.display = 'none';
+});
 
+/* menu dropdown codes for about page....... */
+const openHambugerAbout = document.getElementById('hambuger_about  img');
+const menuAbout = document.getElementById('hide_menu_about');
+const closeButtonAbout = document.getElementById('close_button_about');
+/* open button */
+openHambugerAbout.addEventListener('click', () => {
+  menuAbout.style.display = 'block';
+  openHambugerAbout.style.display = 'none';
+  closeButtonAbout.style.display = 'block';
+});
 
- /* menu dropdown codes.......*/
- const openHambuger=document.querySelector('.hambuger img');
- const menu=document.querySelector('.hide-menu');
- const close_button= document.querySelector('.close_button');
- /*open button */
- openHambuger.addEventListener('click', ()=>{
-    menu.style.display="block";
-    openHambuger.style.display='none'; 
-    close_button.style.display='block';
- })
-
-
- /* clossing button ...*/
- close_button.addEventListener('click', ()=>{
-    menu.style.display="none";
-    openHambuger.style.display='block'; 
-    close_button.style.display='none';
- })
-
- /* menu dropdown codes for about page.......*/
- const openHambugerAbout=document.getElementById('hambuger_about  img');
- const menuAbout=document.getElementById('hide_menu_about');
- const close_buttonAbout= document.getElementById('close_button_about');
- /*open button */
- openHambugerAbout.addEventListener('click', ()=>{
-    menuAbout.style.display="block";
-    openHambugerAbout.style.display='none'; 
-    close_buttonAbout.style.display='block';
- })
-
-
- /* clossing button ...*/
- close_button.addEventListener('click', ()=>{
-    menuAbout.style.display="none";
-    openHambugerAbout.style.display='block'; 
-    close_buttonAbout.style.display='none';
- })
-
-
-
+/* clossing button ... */
+closeButton.addEventListener('click', () => {
+  menuAbout.style.display = 'none';
+  openHambugerAbout.style.display = 'block';
+  closeButtonAbout.style.display = 'none';
+});
